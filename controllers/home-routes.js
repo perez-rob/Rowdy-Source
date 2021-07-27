10199c143fb8c6d9ed7e4516dd96bc9170a61478
bob.w.perez <bob.w.perez@gmail.com>
2021-07-26 22:02:10 -0400

    created error message for invalid enpoints and set up route for homepage with sample HB partials rendering successfully

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const { Project, User } = require('../models');
+// import auth middleware
+
+router.get('/', async (req, res) => {
+  try {
+    const projectData = await Project.findAll({
+      include: [{ model: User }],
+    });
+
+    const projects = projectData.map((project) => project.get({ plain: true }));
+    console.log(projects);
+    res.render('homepage', { projects });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,13 @@ const homeRoutes = require('./home-routes');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
+// might need to edit or comment this out
+router.use('*', (req, res) => {
+  res
+    .status(404)
+    .send(
+      `<h1>${req.originalUrl} is not a valid endpoint for Rowdy Source</h1>`
+    );
+});
+
 module.exports = router;
